0d295cd5087ac34df2470b31695462033106afc2
AzamatKochimov <kochimovazamat180@gmail.com>
2024-04-15 16:08:51 +0500

    Azamat, nothing new

--- lib/common/widgets/app_material_context.dart
+++ lib/common/widgets/app_material_context.dart
@@ -63,7 +63,7 @@ class _AppMaterialContextState extends State<AppMaterialContext> {
                 local.changeLocal(LangCodes.ru);
                 return const Locale('ru', 'RU');
               },
-              initialRoute: "/",
+              initialRoute: "/product_details_page",
               onGenerateRoute: router.onGenerateRoute,
               builder: (context, child) => MediaQuery(
                 data: MediaQuery.of(context)
--- lib/feature/product_details/presentation/pages/product_details_page.dart
+++ lib/feature/product_details/presentation/pages/product_details_page.dart
@@ -1,7 +1,5 @@
 import 'package:dsd/common/styles/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ProductDetailsPage extends StatelessWidget {
@@ -18,17 +16,19 @@ class ProductDetailsPage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {
-              //write share logic here
-            },
-            icon: SvgPicture.asset('assets/icons/share-w.svg', width: 30, height: 30)),
+              onPressed: () {
+                //write share logic here
+              },
+              icon: SvgPicture.asset('assets/icons/share-w.svg',
+                  width: 30, height: 30)),
           IconButton(
             onPressed: () {
               //write add to cart logic here
             },
             icon: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: SvgPicture.asset('assets/icons/cart-w.svg', width: 28, height: 28),
+              child: SvgPicture.asset('assets/icons/cart-w.svg',
+                  width: 28, height: 28),
             ),
           ),
         ],
